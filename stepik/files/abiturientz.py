1bafc27361878fc35a9f1156b37a9e4b867cd3da
This_is_home <sl0nh0ttabuch@gmail.com>
2019-01-25 03:20:37 +0300

    one more

--- stepik/files/abiturientz.py
+++ stepik/files/abiturientz.py
@@ -1,45 +1,22 @@
 # yey :D
-def mid(*q):
 
-    local_string = q[0].split(';')
-    del local_string[0]
-
-
-    w = []
-    [w.append(int(j)) for j in local_string]
-
-
-    if len(q)>1:
-        # print(int(local_string[q[1]]))
-        q[2].append(int(local_string[q[1]]))
-    else:
-        # print(str(sum(w)/3))
-        return round(sum(w)/3, 19)
-# print(mid([1,2,3]))
-
-
-
-stringToPrint = []
-exam0,exam1,exam2=[],[],[]
-
+stringToPrint = ''
+exMarks = [[],[],[]]
 with open('dataset_3363_4.txt', encoding="utf8") as file:
     for string in file:
-        string.strip()
-
-        stringToPrint.append(mid(string))
-
+        listedStr = string.strip().split(';')
+        myCount = 0
+        for j in range(1,4):
+            myCount += int(listedStr[j])
+            exMarks[j-1].append(int(listedStr[j]))
+        stringToPrint += str(myCount/3) + '\n'
         # print(stringToPrint)
-        mid(string,0,exam0)
-        mid(string,1,exam1)
-        mid(string,2,exam2)
-    # print(sum(exam0)/3,sum(exam1)/3,sum(exam2)/3,sep = ' ')
 
 
 
 
-with open('qwqw.txt', 'w') as file:
-    for i in stringToPrint:
-        file.write(str(i)+'\n')
-    file.write(str(round(sum(exam0)/3,11))+' ')
-    file.write(str(round(sum(exam1)/3,11))+' ')
-    file.write(str(round(sum(exam2)/3,91))+' ')
+with open('qwqw.txt', 'w') as qwqw:
+    qwqw.write(stringToPrint)
+    for i in exMarks:
+        stringToPrint = str(sum(i)/len(i)) + ' '
+        qwqw.write(stringToPrint)
